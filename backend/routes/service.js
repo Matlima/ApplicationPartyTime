dbf61b7493691947f287a2c7afc62ac5acc897a7
Matheus <limarios18@outlook.com>
2024-03-13 23:16:28 -0300

    criação do POST de service no database, e adição de request.rest para requisição sem front-end

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 
 const serviceController = require("../controllers/serviceController");
 
-// Funções
+// Funções de end-point para API
+
+router
+    .route("/services")
+    .post((req, res) => serviceController.create(req, res));
 
 module.exports = router;
