9fb948255cf42e6dcfdf34bec3de43365871d293
Matheus <limarios18@outlook.com>
2024-03-13 23:42:22 -0300

    criando method getById para o web service

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -2,12 +2,19 @@ const router = require("express").Router();
 
 const serviceController = require("../controllers/serviceController");
 
-// Funções de end-point para API
-
+//END-POINT POST
 router
     .route("/services")
     .post((req, res) => serviceController.create(req, res));
 
-router.route("/services").get((req, res) => serviceController.getAll(req,res));
+//END-POINT GET ALL
+router
+    .route("/services")
+    .get((req, res) => serviceController.getAll(req,res));
+
+//END-POINT GET ID
+router
+    .route("/services/:id")
+    .get((req, res) => serviceController.get(req, res));
 
 module.exports = router;
