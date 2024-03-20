c3fe6c4ac9438607cc16c50e11dba9d44e9b239d
Matheus <limarios18@outlook.com>
2024-03-19 23:20:02 -0300

    POST da função de party

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -9,6 +9,13 @@ const CreateParty = () => {
 
   const [services, setServices] = useState([]);
 
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [budget, setBudget] = useState(0);
+  const [image, setImage] = useState("");
+  const [partyServices, setPartyServices] = useState([]);
+
   //Load service
   useEffect(() => {
 
@@ -19,33 +26,95 @@ const CreateParty = () => {
     loadServices();
   }, []);
 
+  // Add or remove service
+  const handleService = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
 
+    // Filter do id pelo valor do selecionado
+    const filteredService = services.filter((s) => s._id === value);
 
+    if (checked) {
+      // Adicionar todos que estão selecionados e possuem o mesmo ID
+      setPartyServices((services) => [...services, filteredService[0]]);
+    } else {
+      // Retirar todos que não estão selecionados
+      setPartyServices((services) => services.filter((s) => s._id !== value));
+    }
+
+    console.log(partyServices);
+  };
+
+  // Create a new party
+  const CreateParty = (e) => {
+    e.preventDefault();
+
+    const party = {
+      title,
+      author,
+      description,
+      budget,
+      image,
+      services: partyServices,
+    };
+    
+    console.log(party);
+
+  };
 
   return (
     <div className="form-page">
       <h2>Crie sua próxima Festa:</h2>
       <p>Defina o seu orçamento e escolha os serviços</p>
-      <form>
+      <form onSubmit={(e) => CreateParty(e)}>
         <label>
           <span>Nome da festa:</span>
-          <input type="text" placeholder="Seja criativo..." required/>
+          <input 
+          type="text" 
+          placeholder="Seja criativo..." 
+          required 
+          onChange={(e) => setTitle(e.target.value)} 
+          value={title} 
+          />
         </label>
         <label>
           <span>Anfitrião:</span>
-          <input type="text" placeholder="Quem está dando a festa?" required/>
+          <input 
+          type="text" 
+          placeholder="Quem está dando a festa?" 
+          required
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+          />
         </label>
         <label>
           <span>Descrição:</span>
-          <textarea placeholder="Conte mais sobre a festa..." required></textarea>
+          <textarea 
+          placeholder="Conte mais sobre a festa..." 
+          required
+          onChange={(e) => setDescription(e.target.value)} 
+          value={description} 
+          ></textarea>
         </label>
         <label>
           <span>Orçamento:</span>
-          <input type="number" placeholder="Quanto você pretende investir?" required/>
+          <input 
+          type="number" 
+          placeholder="Quanto você pretende investir?" 
+          required
+          onChange={(e) => setBudget(e.target.value)} 
+          value={budget} 
+          />
         </label>
         <label>
           <span>Imagem:</span>
-          <input type="text" placeholder="Insira a URL de uma imagem" required />
+          <input 
+          type="text" 
+          placeholder="Insira a URL de uma imagem" 
+          required 
+          onChange={(e) => setImage(e.target.value)} 
+          value={image} 
+          />
         </label>
         <label>
           <div>
@@ -58,7 +127,11 @@ const CreateParty = () => {
                   <p className="service-name">{service.name}</p>
                   <p className="service-price">R${service.price}</p>
                   <div className="checkbox-container">
-                    <input type="checkbox" value={service._id} />
+                    <input 
+                    type="checkbox" 
+                    value={service._id}
+                    onChange={(e) => handleService(e)}
+                    />
                     <p>Marque para solicitar</p>
                   </div>
                 </div>
