3a45217ca9aa46aac033f42d3e6e6a55660cee54
Matheus <limarios18@outlook.com>
2024-03-20 11:42:44 -0300

    adicionando pagina de party individual

--- frontend/src/routes/Party.jsx
+++ frontend/src/routes/Party.jsx
@@ -1,51 +1,42 @@
 import partyFetch from "../axios/config";
-
 import { useState, useEffect } from "react";
-
 import { useParams, Link, useNavigate } from "react-router-dom"; 
 
+import "./Party.css";
+
 const Party = () => {
-
     const { id } = useParams();
-
-    const [party, setParty] = useState(null);
-
+    const [party, setParty] = useState({});
     const navigate = useNavigate();
 
     // Load party
     useEffect(() => {
-        const loadParty = async() => {
+        const loadParty = async () => {
             const res = await partyFetch.get(`/parties/${id}`);
-
-            console.log(res.data);
-
             setParty(res.data);
         }
         loadParty();
     }, []);
 
-    
-    
-  return (
-    <div className="party">
-        <h1>{party.title}</h1>
-        <div className="actions-container">
-            <Link className="btn">Editar</Link>
-            <button className="btn-secondary">Excluir</button>
+    return (
+        <div className="party">
+            <h1>{party.title}</h1>
+            <div className="actions-container">
+                <Link className="btn">Editar</Link>
+                <button className="btn-secondary">Excluir</button>
+            </div>
+            <p>Orçamento: R${party.budget}</p>
+            <h3>Serviços contratados:</h3>
+            <div className="services-container">
+                {party.services && party.services.map((service) => (
+                    <div className="service" key={service._id}>
+                        <img src={service.image} alt={service.name} />
+                        <p>{service.name}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-        <p>Orçamento: R${party.budget}</p>
-        <h3>Serviços contratados:</h3>
-        <div className="services-container">
-            {party.services.map((service) => (
-                <div className="service" key={service._id}>
-                    <img src={service.image} alt={service.name} />
-                    <p>{service.name}</p>
-                </div>
-            ))}
-        </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Party;
